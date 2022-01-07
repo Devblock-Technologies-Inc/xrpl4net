8bc933792c222a3037fb937708f50d586fe6e80c
TrongHuynh <huynhhuutrong2271989@gmail.com>
2022-01-07 15:18:55 +0700

    add test

--- src/xrpl4net.tests/Client/SendXRPTests.cs
+++ src/xrpl4net.tests/Client/SendXRPTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xrpl4net.Client.Models.Accounts;
+using Xrpl4net.Client.Models.KeyGeneration;
 using Xrpl4net.Client.Models.Ledger;
 using Xrpl4net.Client.Models.ServerInfo;
 
@@ -21,56 +22,65 @@ namespace Xrpl4net.Tests.Client
         [Test]
         public async Task SendXRP()
         {
-            // Prepare transaction --------------------------------------------------------
-            // Look up your Account Info
-            var accountInfoRequest = new AccountInfoRequest
+            try
             {
-                Params = new List<AccountInfoRequestParams>
+                // Prepare transaction --------------------------------------------------------
+                
+                // Look up your Account Info
+                var accountInfoRequest = new AccountInfoRequest
                 {
-                    new AccountInfoRequestParams
+                    Params = new List<AccountInfoRequestParams>
                     {
-                        account = "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn",
-                        strict = true,
-                        ledger_index = "current",
-                        queue = true
-                    }
-                }.ToArray()
-            };
+                        new AccountInfoRequestParams
+                        {
+                            account = "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn",
+                            strict = true,
+                            ledger_index = "current",
+                            queue = true
+                        }
+                    }.ToArray()
+                };
 
-            var accountInfoResponse = await _xrplClient.AccountInfo(accountInfoRequest);
-            var accountInfoResult = accountInfoResponse.result;
-            var sequence = accountInfoResult.account_data.Sequence;
+                var accountInfoResponse = await _xrplClient.AccountInfo(accountInfoRequest);
+                var accountInfoResult = accountInfoResponse.result;
+                var sequence = accountInfoResult.account_data.Sequence;
 
-            // Request current fee information from rippled
-            var feeResponse = await _xrplClient.Fee(new FeeRequest());
-            var feeResult = feeResponse.result;
-            var openLedgerFee = feeResult.drops.open_ledger_fee;
+                // Request current fee information from rippled
+                var feeResponse = await _xrplClient.Fee(new FeeRequest());
+                var feeResult = feeResponse.result;
+                var openLedgerFee = feeResult.drops.open_ledger_fee;
 
-            // Get the latest validated ledger index
-            var ledgerRequest = new LedgerRequest
+                // Get the latest validated ledger index
+                var ledgerRequest = new LedgerRequest
+                {
+                    Params = new List<LedgerRequestParams>
+                    {
+                        new LedgerRequestParams
+                        {
+                            accounts = false,
+                            full = false,
+                            ledger_index = "validated",
+                            transactions = false,
+                            expand = false,
+                            owner_funds = false
+                        }
+                    }.ToArray()
+                };
+
+                var ledgerResponse = await _xrplClient.Ledger(ledgerRequest);
+                var ledgerResult = ledgerResponse.result;
+                var validatedLedger = ledgerResult.ledger_index;
+
+                // Construct a Payment
+
+
+                // Sign transaction -----------------------------------------------------------
+
+            }
+            catch (Exception ex)
             {
-                Params = new List<LedgerRequestParams>
-                {
-                    new LedgerRequestParams
-                    {
-                        accounts = false,
-                        full = false,
-                        ledger_index = "validated",
-                        transactions = false,
-                        expand = false,
-                        owner_funds = false
-                    }
-                }.ToArray()
-            };
-
-            var ledgerResponse = await _xrplClient.Ledger(ledgerRequest);
-            var ledgerResult = ledgerResponse.result;
-            var validatedLedger = ledgerResult.ledger_index;
-
-            // Construct a Payment
-
-
-            // Sign transaction -----------------------------------------------------------
+                throw new Exception(ex.Message);
+            }
 
         }
     }
